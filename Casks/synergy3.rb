b779f7e334e6932b3224f95e3bfc38e1398f1212
zhiyue <cszhiyue@gmail.com>
2025-03-07 01:56:58 +0800

    feat(casks): 添加 Synergy3 应用的 Cask 文件
    - 创建新的 Cask 文件以支持 Synergy3 应用
    - 包含 ARM 和 Intel 架构的下载链接和 checksum
    - 设置应用的描述、主页和版本信息
    - 配置自动更新和依赖条件
    - 定义安装、卸载和清理操作

---
+++ Casks/synergy3.rb
@@ -0,0 +1,47 @@
+cask "synergy3" do
+  version "3.2.1"
+
+  on_arm do
+    sha256 "e1dd69fb158235c42b4cd39c41351d3107bd8d918bb80ecd4d4214b4671102bd"
+    url "https://symless.com/synergy/synergy/api/download/synergy-#{version}-macos-arm64.dmg"
+  end
+
+  on_intel do
+    sha256 "bf4de22746c1544e9893fb97772f008b99c6bb085cb64614fc749ef17b4b5b37"
+    url "https://symless.com/synergy/synergy/api/download/synergy-#{version}-macos-x64.dmg"
+  end
+
+  name "Synergy"
+  desc "Share one mouse and keyboard between multiple computers"
+  homepage "https://symless.com/synergy"
+
+  livecheck do
+    url "https://symless.com/synergy/download"
+    strategy :page_match
+    regex(/v(\d+(?:\.\d+)+)/i)
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+  conflicts_with cask: "synergy"
+
+  app "Synergy.app"
+
+  uninstall quit:      "com.symless.synergy",
+            launchctl: "com.symless.synergy3",
+            delete:    [
+              "/Applications/Synergy.app",
+              "/Library/PrivilegedHelperTools/synergy-core"
+            ]
+
+  zap trash: [
+    "~/Library/Application Support/synergy",
+    "~/Library/Application Support/CrashReporter/synergy_*.plist",
+    "~/Library/LaunchAgents/com.symless.synergy3.plist",
+    "~/Library/Logs/Synergy",
+    "~/Library/Logs/DiagnosticReports/synergy-core-*.ips",
+    "~/Library/Preferences/com.symless.synergy.plist",
+    "~/Library/Preferences/Synergy",
+    "~/Library/Saved Application State/com.symless.synergy.savedState"
+  ]
+end
