2b05bf08345a0d42495530cf9855bc0d3e7e7403
zhiyue <cszhiyue@gmail.com>
2025-03-07 16:46:04 +0800

    refactor(synergy3): 重构 Cask 文件并优化 GitHub Actions 工作流
    - 重构 synergy3.rb 文件，使用 arch 变量替代 on_arm 和 on_intel 块
    - 更新 GitHub Actions 工作流，使 tap 语法检查继续执行即使出错
    - 优化 Cask 文件结构，提高可读性和维护性

--- Casks/synergy3.rb
+++ Casks/synergy3.rb
@@ -1,16 +1,10 @@
 cask "synergy3" do
+  arch arm: "arm64", intel: "x64"
   version "3.2.1"
+  sha256 arm:   "e1dd69fb158235c42b4cd39c41351d3107bd8d918bb80ecd4d4214b4671102bd",
+         intel: "bf4de22746c1544e9893fb97772f008b99c6bb085cb64614fc749ef17b4b5b37"
 
-  on_arm do
-    sha256 "e1dd69fb158235c42b4cd39c41351d3107bd8d918bb80ecd4d4214b4671102bd"
-    url "https://symless.com/synergy/synergy/api/download/synergy-#{version}-macos-arm64.dmg"
-  end
-
-  on_intel do
-    sha256 "bf4de22746c1544e9893fb97772f008b99c6bb085cb64614fc749ef17b4b5b37"
-    url "https://symless.com/synergy/synergy/api/download/synergy-#{version}-macos-x64.dmg"
-  end
-
+  url "https://symless.com/synergy/synergy/api/download/synergy-#{version}-macos-#{arch}.dmg"
   name "Synergy"
   desc "Share one mouse and keyboard between multiple computers"
   homepage "https://symless.com/synergy"
