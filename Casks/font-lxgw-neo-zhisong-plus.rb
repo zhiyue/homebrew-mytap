583e59cb2b031c9a3caea2e38e05eaac7adeca29
zhiyue <cszhiyue@gmail.com>
2025-03-24 18:20:23 +0800

    feat(fonts): 添加 Lxgw Neo ZhiSong 字体系列
    - 新增 font-lxgw-neo-zhisong-plus 字体
    - 新增 font-lxgw-neo-zhisong 字体
    - 这两个字体是基于霞鹜文楷的衍生字体，具有不同的样式和用途

---
+++ Casks/font-lxgw-neo-zhisong-plus.rb
@@ -0,0 +1,18 @@
+cask "font-lxgw-neo-zhisong-plus" do
+  version "1.028.1"
+  sha256 "1BA321BCA06605D13BE35C7BEC38E18F88D527D08E8030E1CE25A06B6C785CC9"  
+
+  url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v#{version}/LXGWNeoZhiSongPlus.ttf"
+  name "LXGW Neo ZhiSong Plus"
+  name "霞鹜新致宋 Plus"
+  desc "开源中文字体，基于霞鹜文楷的衍生字体增强版本"
+  homepage "https://github.com/lxgw/LxgwNeoZhiSong"
+
+  livecheck do
+    url "https://github.com/lxgw/LxgwNeoZhiSong/releases"
+    strategy :github_releases
+    regex(/^v?(\d+(?:\.\d+)+(?:\.\d+)?)$/i)
+  end
+
+  font "LXGWNeoZhiSongPlus.ttf"
+end
---
+++ Casks/font-lxgw-neo-zhisong.rb
@@ -0,0 +1,18 @@
+cask "font-lxgw-neo-zhisong" do
+  version "1.028.1"
+  sha256 "CA31DC1A9A2AF13BFA95297B59D10EE7CA14C7701B765E141183F173CB9652F6"  
+
+  url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v#{version}/LXGWNeoZhiSong.ttf"
+  name "LXGW Neo ZhiSong"
+  name "霞鹜新致宋"
+  desc "开源中文字体，基于霞鹜文楷的衍生字体"
+  homepage "https://github.com/lxgw/LxgwNeoZhiSong"
+
+  livecheck do
+    url "https://github.com/lxgw/LxgwNeoZhiSong/releases"
+    strategy :github_releases
+    regex(/^v?(\d+(?:\.\d+)+(?:\.\d+)?)$/i)
+  end
+
+  font "LXGWNeoZhiSong.ttf"
+end
