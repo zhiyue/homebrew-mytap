f4b2a25e9ccd5bbf6417eca1533508e6f8b838d7
zhiyue <cszhiyue@gmail.com>
2025-05-16 02:34:54 +0000

    font-lxgw-neo-zhisong-plus 1.030.2

--- Casks/font-lxgw-neo-zhisong-plus.rb
+++ Casks/font-lxgw-neo-zhisong-plus.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-neo-zhisong-plus" do
-  version "1.030.1"
-  sha256 "cd1df4c6d086fbcd9da706014344451ad666edae5d72fd816640fa9eca1dc38e"  
+  version "1.030.2"
+  sha256 "dea4792d71951ba71a8b2abdfb4305a7529a69b24cc351053efa168fcf64955d"  
 
   url "https://github.com/lxgw/LxgwNeoZhiSong/releases/download/v#{version}/LXGWNeoZhiSongPlus.ttf"
   name "LXGW Neo ZhiSong Plus"
